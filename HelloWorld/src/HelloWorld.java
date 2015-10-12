ffcc25b3274edf73863a5d59d45998817bcca085
Tobias Wüchner <wuechner@in.tum.de>
2015-10-12 13:35:00 +0200

    initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tobias
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello ASE2015 - how are you? :)");
+	}
+
+}
